ee4a411bd919c2346ae937f8758397820cb57628
Daniel Sikar <dsikar@tycoint.com>
2017-08-24 18:40:54 +0100

    Added Xbee examples.

---
+++ KinectSimpleGesture/KinectSimpleGesture/GesturePartResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace KinectSimpleGesture
+{
+    /// <summary>
+    /// Represents the gesture part recognition result.
+    /// </summary>
+    public enum GesturePartResult
+    {
+        /// <summary>
+        /// Gesture part failed.
+        /// </summary>
+        Failed,
+
+        /// <summary>
+        /// Gesture part succeeded.
+        /// </summary>
+        Succeeded
+    }
+}
