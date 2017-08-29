a885aefcdf882759f4f360f246be098228bba6b7
Daniel Sikar <dsikar@gmail.com>
2017-08-29 09:16:08 +0100

    Web read/write script for IMU sensor data.

---
+++ cloud/studio-ini.php
@@ -0,0 +1,27 @@
+<?php
+/*
+
+        studio-ini.php
+        Get or set remote BNO055 sensor readings.
+        This page services the write requests from sensors and read
+        requests from PLC client.
+*/
+
+// Parse action, sensor and payload as required
+
+$action = (isset($_GET['action']) ? $_GET['action'] : null);
+$sensor = $_GET['sensor'];
+if ($action == "r") {
+        $cmd = "tail -1 /tmp/" . $sensor . "log.txt";
+        $cmdout = shell_exec($cmd);
+        echo $cmdout;
+} elseif ($action == "w") {
+        $payload = (isset($_GET['payload']) ? $_GET['payload'] : null);
+        $cmd = "echo \"$payload\" >> /tmp/" . $sensor . "log.txt";
+        echo $cmd;
+        shell_exec($cmd);
+} else {
+        echo "Malformed URL - action=(w|r) required.";
+}
+
+?>
