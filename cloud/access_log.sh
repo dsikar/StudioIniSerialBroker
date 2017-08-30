8bb75cf4f47fbdef18757fa58fb6677287616974
Daniel Sikar <dsikar@gmail.com>
2017-08-30 12:12:15 +0100

    Bash script clean-up.

--- cloud/access_log.sh
+++ cloud/access_log.sh
@@ -5,7 +5,7 @@
 sudo chmod 755 /var/log/apache2
 sudo chmod 755 /var/log/apache2/access.log
 
-# Change access so www-data can right to /tmp/graffiti.txt
+# Change access so www-data can write to /tmp/*log.txt
 
 sudo chmod 777 /tmp/1log.txt
 sudo chmod 777 /tmp/2log.txt
